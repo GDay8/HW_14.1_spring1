60470e09e593ff3de395c04fff8db9c271efd883
GDay8 <Derdyukim@gmail.com>
2023-06-18 08:46:57 +0300

    Revert "Initial version"
    This reverts commit ba15df924cb6119ba4c60cd3583510a8bf6d3de4.

--- students_group/src/main/java/com/hw14_1/students_groups/repository/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hw14_1.students_groups.repository;
-
-import com.hw14_1.students_groups.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends JpaRepository <Group, Integer> {
-}
